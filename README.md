e833a41fd37e18804b4562e2189da5f59fd477ae
Brad Campbell <bradjc5@gmail.com>
2023-11-09 10:40:16 -0500

    config file, remove update
    - Use INI config file, easier to separate config from code.
    - Create dicts directly to avoid having to delete keys
    - Format with black
    - Use shorter names to reduce line wrapping
    - Support another gateway type with fewer fields
    - Update readme
    - Use virtual env

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# ttn-eventsapi-mqtt
- Python application to connect to TTN events API and push data to Influx MQTT
+ttn-eventsapi-mqtt
+==================
+
+Python application to connect to TTN events API and push data to MQTT.
+
+Setup and Installation
+----------------------
+
+Setup virtual environment and install dependencies:
+
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+Create a configuration .ini file using the format below.
+
+For each gateway create a `[gateway X]` section. The `[section]` names can be
+anything as long as they are unique and start with `gateway`.
+
+For each gateway you need an API key from TTN. One the Add API key page you need
+to select the following permissions:
+
+- View gateway information
+- View gateway status
+- Read gateway traffic
+
+Configuration file template:
+
+```
+[mqtt]
+topic = <topic>
+broker = <broker url>
+port = 8883
+username = <username>
+password = <port>
+client_id = gateway-info-stream
+
+[ttn]
+server = https://nam1.cloud.thethings.network/api/v3/events
+
+[gateway 1]
+name = <gateway-name>
+id = <gateway-id>
+key = <api key>
+
+[gateway 2]
+name = <gateway-name>
+id = <gateway-id>
+key = <api key>
+```
+
+Run the script from within the virtual environment:
+
+```bash
+./async-ttn-eventsapi-mqtt.py <path to configuration file>
+```
--- async-ttn-eventsapi-mqtt.py
+++ async-ttn-eventsapi-mqtt.py
@@ -1,141 +1,208 @@
 #!/usr/bin/env python3
 
 import asyncio
-import httpx
+import configparser
 import json
+import sys
+
+import httpx
 import paho.mqtt.client as mqtt
 
-import data_format
-import mqtt_secrets
-import ttn_secrets
+# Data format for specific packet reception.
+data_dict_up_receive = {
+    "raw_payload_size_bytes": 0,
+    "rssi_dbm": 0.0,
+    "rssi": 0.0,
+    "snr_db": 0.0,
+    "snr": 0.0,
+    "lora_f_cnt": 0,
+    "bandwidth_hz": 0,
+    "spreading_factor": 0,
+    "frequency_hz": 0.0,
+    "lora_dev_addr": "",
+    "latitude_degrees": 30.1,
+    "longitude_degrees": -70.1,
+    "altitude_m": 2,
+    "_meta": {
+        "received_time": "",
+        "device_id": "7",
+        "platform": "cisco7",
+        "band_id": "US_902_928",
+        "receiver": "ttn-eventsapi-mqtt",
+    },
+}
 
+# Data format for gateway statistics.
+data_dict_gs_stats = {
+    "uplink_count": 0,
+    "downlink_count": 0,
+    "tx_acknowledgment_count": 0,
+    "downlink_utilization_band1": 0.0,
+    "_meta": {
+        "received_time": "",
+        "device_id": "7",
+        "platform": "cisco",
+        "min_frequency_band1": "923300000",
+        "max_frequency_band1": "927500000",
+        "receiver": "ttn-eventsapi-mqtt",
+    },
+}
 
 
 def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-                print('Connected to MQTT broker\n')
-                pass
-        else:
-                print('Failed to connect to MQTT broker, reason is {}\n'.format(rc))
-                pass
+    if rc == 0:
+        print("Connected to MQTT broker")
+    else:
+        print("Failed to connect to MQTT broker, reason is {}".format(rc))
 
 
-async def gateway_info_stream(queue,TTN_SERVER,GATEWAY_ID,TTN_KEY):
+def create_pkt_stats(msg):
+    pkt = {"_meta": {"receiver": "ttn-eventsapi-mqtt"}}
 
-    data_dict_up_receive = data_format.data_dict_up_receive
-    data_dict_gs_stats = data_format.data_dict_gs_stats
+    # Data
+    pkt["uplink_count"] = int(msg["result"]["data"]["uplink_count"])
+    pkt["downlink_count"] = int(msg["result"]["data"]["downlink_count"])
+    pkt["tx_acknowledgment_count"] = int(
+        msg["result"]["data"]["tx_acknowledgment_count"]
+    )
+    if "downlink_utilization" in msg["result"]["data"]["sub_bands"][1]:
+        pkt["downlink_utilization_band1"] = float(
+            msg["result"]["data"]["sub_bands"][1]["downlink_utilization"]
+        )
 
+    # Metadata
+    pkt["_meta"]["received_time"] = str(msg["result"]["time"])
+    pkt["_meta"]["device_id"] = msg["result"]["identifiers"][0]["gateway_ids"]["eui"]
+    if "platform" in msg["result"]["data"]["last_status"]["versions"]:
+        pkt["_meta"]["platform"] = msg["result"]["data"]["last_status"]["versions"][
+            "platform"
+        ]
+    pkt["_meta"]["min_frequency_band1"] = int(
+        msg["result"]["data"]["sub_bands"][1]["min_frequency"]
+    )
+    pkt["_meta"]["max_frequency_band1"] = int(
+        msg["result"]["data"]["sub_bands"][1]["max_frequency"]
+    )
+
+    return pkt
+
+
+def create_pkt_receive(rst):
+    pkt = {"_meta": {"receiver": "ttn-eventsapi-mqtt"}}
+
+    msg = rst["result"]["data"]["message"]
+
+    raw_payload = msg["raw_payload"]
+    # Calculate size in bytes from base64 format
+    raw_payload_size_bytes = (3 - raw_payload[-4:].count("=")) + (
+        len(raw_payload[:-4]) / 4
+    ) * 3
+
+    pkt["raw_payload_size_bytes"] = int(raw_payload_size_bytes)
+    pkt["rssi_dbm"] = int(msg["rx_metadata"][0]["rssi"])
+    pkt["rssi"] = int(msg["rx_metadata"][0]["rssi"])
+    pkt["bandwidth_hz"] = int(msg["settings"]["data_rate"]["lora"]["bandwidth"])
+    pkt["spreading_factor"] = int(
+        msg["settings"]["data_rate"]["lora"]["spreading_factor"]
+    )
+    pkt["frequency_hz"] = int(msg["settings"]["frequency"])
+    pkt["latitude_degrees"] = float(msg["rx_metadata"][0]["location"]["latitude"])
+    pkt["longitude_degrees"] = float(msg["rx_metadata"][0]["location"]["longitude"])
+    pkt["altitude_m"] = float(msg["rx_metadata"][0]["location"]["altitude"])
+
+    if "mac_payload" in msg["payload"]:
+        if "f_cnt" in msg["payload"]["mac_payload"]["f_hdr"]:
+            pkt["lora_f_cnt"] = int(msg["payload"]["mac_payload"]["f_hdr"]["f_cnt"])
+
+        if "dev_addr" in msg["payload"]["mac_payload"]["f_hdr"]:
+            pkt["lora_dev_addr"] = msg["payload"]["mac_payload"]["f_hdr"]["dev_addr"]
+
+    if "snr" in msg["rx_metadata"][0]:
+        pkt["snr_db"] = int(msg["rx_metadata"][0]["snr"])
+        pkt["snr"] = int(msg["rx_metadata"][0]["snr"])
+
+    pkt["_meta"]["received_time"] = msg["rx_metadata"][0]["received_at"]
+    pkt["_meta"]["device_id"] = rst["result"]["identifiers"][0]["gateway_ids"]["eui"]
+    pkt["_meta"]["band_id"] = rst["result"]["data"]["band_id"]
+
+    return pkt
+
+
+async def gateway_info_stream(queue, ttn_server, gateway_id, ttn_key):
     async with httpx.AsyncClient() as client:
-        async with client.stream("POST",TTN_SERVER,
-                                headers={'Authorization': 'Bearer '+TTN_KEY , 'Accept': 'text/event-stream', 'Content-type': 'application/json; charset=utf-8'},
-                                data='{"identifiers":[{"gateway_ids":{"gateway_id":"'+GATEWAY_ID+'"}}]}', timeout=None) as r:
-
+        async with client.stream(
+            "POST",
+            ttn_server,
+            headers={
+                "Authorization": "Bearer " + ttn_key,
+                "Accept": "text/event-stream",
+                "Content-type": "application/json; charset=utf-8",
+            },
+            data='{{"identifiers":[{{"gateway_ids":{{"gateway_id":"{}"}}}}]}}'.format(
+                gateway_id
+            ),
+            timeout=None,
+        ) as r:
             async for line in r.aiter_text():
                 try:
                     message = json.loads(line)
                     if message["result"]["name"] == "gs.gateway.connection.stats":
-                            data_dict_gs_stats.update({
-                                    "uplink_count": int(message["result"]["data"]["uplink_count"]),
-                                    "downlink_count": int(message["result"]["data"]["downlink_count"]),
-                                    "tx_acknowledgment_count": int(message["result"]["data"]["tx_acknowledgment_count"]),
-                                    "downlink_utilization_band1": float(message["result"]["data"]["sub_bands"][1]["downlink_utilization"]),
-
-                                    "_meta": {"received_time": str(message["result"]["time"]),
-                                                      "device_id": str(message["result"]["identifiers"][0]["gateway_ids"]["eui"]),
-                                              "platform": str(message["result"]["data"]["last_status"]["versions"]["platform"]),
-                                              "min_frequency_band1": int(message["result"]["data"]["sub_bands"][1]["min_frequency"]),
-                                              "max_frequency_band1": int(message["result"]["data"]["sub_bands"][1]["max_frequency"]),
-                                              "receiver": "ttn-eventsapi-mqtt"}
-                            })
-
-                            await queue.put(data_dict_gs_stats)
+                        await queue.put(create_pkt_stats(message))
 
                     if message["result"]["name"] == "gs.up.receive":
-                            raw_payload = message["result"]["data"]["message"]["raw_payload"]
-                            raw_payload_size_bytes = (3-raw_payload[-4:].count('='))+(len(raw_payload[:-4])/4)*3  #calculate size in bytes from base64 format
-                            data_dict_up_receive.update({
-                                    "raw_payload_size_bytes": int(raw_payload_size_bytes),
-                                    "rssi_dbm": int(message["result"]["data"]["message"]["rx_metadata"][0]["rssi"]),
-                                    "rssi": int(message["result"]["data"]["message"]["rx_metadata"][0]["rssi"]),
-                                    "bandwidth_hz":int(message["result"]["data"]["message"]["settings"]["data_rate"]["lora"]["bandwidth"]),
-                                    "spreading_factor":int(message["result"]["data"]["message"]["settings"]["data_rate"]["lora"]["spreading_factor"]),
-                                    "frequency_hz":int(message["result"]["data"]["message"]["settings"]["frequency"]),
-                                    "latitude_degrees": float(message["result"]["data"]["message"]["rx_metadata"][0]["location"]["latitude"]),
-                                    "longitude_degrees": float(message["result"]["data"]["message"]["rx_metadata"][0]["location"]["longitude"]),
-                                    "altitude_m": float(message["result"]["data"]["message"]["rx_metadata"][0]["location"]["altitude"])
-                            })
-
-                            if "mac_payload" in message["result"]["data"]["message"]["payload"]:
-                                    if "f_cnt" in message["result"]["data"]["message"]["payload"]["mac_payload"]["f_hdr"]:
-                                            data_dict_up_receive.update({"lora_f_cnt":int(message["result"]["data"]["message"]["payload"]["mac_payload"]["f_hdr"]["f_cnt"])})
-                                    else:
-                                            if "lora_f_cnt" in data_dict_up_receive:
-                                                    del data_dict_up_receive["lora_f_cnt"]
-                                    if "dev_addr" in message["result"]["data"]["message"]["payload"]["mac_payload"]["f_hdr"]:
-                                            data_dict_up_receive.update({"lora_dev_addr":str(message["result"]["data"]["message"]["payload"]["mac_payload"]["f_hdr"]["dev_addr"])})
-                                    else:
-                                            if "lora_dev_addr" in data_dict_up_receive:
-                                                    del data_dict_up_receive["lora_dev_addr"]
-
-                            if "snr" in message["result"]["data"]["message"]["rx_metadata"][0]:
-                                    data_dict_up_receive.update({"snr_db": int(message["result"]["data"]["message"]["rx_metadata"][0]["snr"]),
-                                            "snr": int(message["result"]["data"]["message"]["rx_metadata"][0]["snr"])})
-                            else:
-                                    if "snr_db" in data_dict_up_receive:
-                                            del data_dict_up_receive["snr_db"]
-                                            del data_dict_up_receive["snr"]
-
-                            data_dict_up_receive["_meta"].update({
-                                    "received_time": str(message["result"]["data"]["message"]["rx_metadata"][0]["received_at"]),
-                                    "device_id" : str(message["result"]["identifiers"][0]["gateway_ids"]["eui"]),
-                                    "band_id" : str(message["result"]["data"]["band_id"]),
-                                    "receiver": "ttn-eventsapi-mqtt"
-                            })
-
-                            await queue.put(data_dict_up_receive)
+                        await queue.put(create_pkt_receive(message))
 
                 except KeyboardInterrupt:
-                        print('<< User stream interrupt >>')
-                except:
-                        print("<< error handling TTN message >>")
+                    print("<< User stream interrupt >>")
+                except Exception as e:
+                    print("<< error handling TTN message >>")
+                    print(e)
+                    print(message)
 
 
 async def main():
+    # First argument is the path to the config file.
+    config_path = sys.argv[1]
 
-    # Initialize mqtt client to push data to DB 
-    MQTT_TOPIC_NAME = mqtt_secrets.MQTT_TOPIC_NAME
-    MQTT_BROKER = mqtt_secrets.MQTT_BROKER
-    MQTT_PORT = mqtt_secrets.MQTT_PORT
-    username = mqtt_secrets.MQTT_USER
-    password = mqtt_secrets.MQTT_PASSWORD
-    client_id = mqtt_secrets.client_id
+    # Read in the INI file to get all of the configuration values.
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    # Group all gateway information from all [gateway X] sections.
+    gateways = []
+    for section in config.sections():
+        if section.startswith("gateway"):
+            gateways.append(config[section])
 
     # Connect to MQTT broker
-    print('conecting to mqtt broker...')
-    client = mqtt.Client(client_id)
-    client.username_pw_set(username, password)
+    print("Connecting to MQTT broker...")
+    client = mqtt.Client(config["mqtt"]["client_id"])
+    client.username_pw_set(config["mqtt"]["username"], config["mqtt"]["password"])
     client.tls_set()
     client.on_connect = on_connect
-    client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    client.loop_start() 
+    client.connect(config["mqtt"]["broker"], int(config["mqtt"]["port"]), 60)
 
     # Load gateway information
-    queue = asyncio.Queue()
-    TTN_SERVER = ttn_secrets.TTN_SERVER
-    GATEWAYS = ttn_secrets.GATEWAYS
-    ID_list = [gateway['GATEWAY_ID'] for gateway in GATEWAYS]
-    KEY_list = [gateway['TTN_KEY'] for gateway in GATEWAYS]
+    ttn_server = config["ttn"]["server"]
+    gateway_ids = [gateway["id"] for gateway in gateways]
+    gateway_keys = [gateway["key"] for gateway in gateways]
 
     # Start async tasks
-    print('starting gateway data stream...')
-    asyncio.gather(*[gateway_info_stream(queue,TTN_SERVER,ID,KEY) for (ID,KEY) in zip(ID_list,KEY_list)])
+    print("Starting gateway data stream...")
+    queue = asyncio.Queue()
+    asyncio.gather(
+        *[
+            gateway_info_stream(queue, ttn_server, id, key)
+            for (id, key) in zip(gateway_ids, gateway_keys)
+        ]
+    )
 
     # Start mqtt main loop
     while True:
         message = await queue.get()
-        status = client.publish(MQTT_TOPIC_NAME, json.dumps(message))
+        status = client.publish(config["mqtt"]["topic"], json.dumps(message))
         # print(json.dumps(message))
         queue.task_done()
-        
+
 
 asyncio.run(main())
